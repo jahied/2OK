8494f46927ff3ac9f5005e374140e3834231543b
Bd jahied <79805259+jahied@users.noreply.github.com>
2022-10-06 16:24:22 +0600

    Update world2.py

--- world2.py
+++ world2.py
@@ -1,14 +1,14 @@
 #Auther Alienrazor  
  #python2 script 2022 
   
- W = '\033[97;1m' 
- R = '\033[91;1m' 
- G = '\033[92;1m' 
- Y = '\033[93;1m' 
- B = '\033[94;1m' 
- P = '\033[95;1m' 
- C = '\033[96;1m' 
- N = '\x1b[0m' 
+ 
+
+ 
+
+ 
+ 
+ 
+ 
   
  import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass 
  os.system('rm -rf .txt') 
