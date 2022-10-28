85e06900af472abfdeb0fec016a85ed153c238d9
Bd jahied <79805259+jahied@users.noreply.github.com>
2022-10-28 19:56:02 +0600

    Update update-2.py

--- update-2.py
+++ update-2.py
@@ -1341,7 +1341,7 @@ def Subscraption():
 		print("")
 		print(" Copy And Send Key To Admin")
 		print ("")
-		print (" Your Key : "+bh+team+key1)
+		print (" Your Key : "+ak+ah+key1)
 		print ("")
 		name = input (" Your Name : ")
 		print ("")
