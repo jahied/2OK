7517195ae7b2f7004c9be036650dba558764d482
Bd jahied <79805259+jahied@users.noreply.github.com>
2022-10-28 19:44:07 +0600

    Update update-2.py

--- update-2.py
+++ update-2.py
@@ -1321,7 +1321,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get(''https://github.com/jah").text
+	r1=requests.get("https://github.com/jahied/EC-170/blame/main/Ac.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
